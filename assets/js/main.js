a0f7bbda8e12b68f2ebde5a678b986bb5788e20b
baziotabeans <1000017079@ucan.edu>
2022-03-22 16:41:03 +0100

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+const vs_code = document.querySelector(".vs-code");
+const vs_code_setting_bar = document.querySelector(".vs-code-setting-bar");
+
+console.log(vs_code_setting_bar);
+
+console.log(vs_code);
+
+dragElement(vs_code);
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.querySelector(".vs-code-setting-bar")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.querySelector(".vs-code-setting-bar").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
